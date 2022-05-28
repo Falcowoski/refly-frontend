f43c5d0ee37e64cdcbf6e1fa1f4ca22b98f89d7d
Leonardo Falcoski <leonardo.falcoski@gmail.com>
2022-05-28 11:35:13 -0300

    feat: Adicionada navegação com React Router no Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { NavLink as RouterNavLink } from "react-router-dom";
 
 function Header() {
     return (
@@ -7,9 +8,9 @@ function Header() {
                 <Navbar.Brand className="">Refly - Metamorfose Digital</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link>Item 1</Nav.Link>
-                        <Nav.Link>Item 2</Nav.Link>
+                    <Nav defaultActiveKey="/" className="ms-auto">
+                        <Nav.Link as={RouterNavLink} to="/">Home</Nav.Link>
+                        <Nav.Link as={RouterNavLink} to="/create">Create</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item>Item 3.1</NavDropdown.Item>
                             <NavDropdown.Item>Item 3.2</NavDropdown.Item>
