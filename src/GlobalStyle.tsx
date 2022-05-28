7137f72b57614ef6f7a2a1895726dd0e95bb9495
Leonardo Falcoski <leonardo.falcoski@gmail.com>
2022-05-28 11:35:47 -0300

    refactor: Removido CSS desnecessário do componente 'GlobalStyle'

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,31 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  /* * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    }
-  ul {
-    list-style: none;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  img {
-    width: 100%;
-    height: auto;
-  }
-  html {
-    scroll-behavior: smooth;
-  }
-  body {
-    font: 400 62.5% 'Montserrat', sans-serif;
-    color: #000000;
-    background: #F8F8F8;
-    -webkit-font-smoothing: antialiased;
-  } */
+// TODO: CUSTOM COLORS AN DARK MODE
 `;
 
 export default GlobalStyle;
